344fc052c06fc2c790d80f90ae8f4c6eba303818
84388 <datnvph30269@fpt.edu.vn>
2023-10-16 20:54:22 +0700

    fist commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  TextInput,
+  FlatList,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const data = [
+  { id: "1", name: "Item 1" },
+  { id: "2", name: "Item 2" },
+  { id: "3", name: "Item 3" },
+];
+const Lab8 = () => {
+  const [searchText, setSearchText] = useState("");
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    return (
+      <View style={styles.container}>
+        <View style={styles.view}>
+          <Text style={styles.text}>Đây là lab 8 dùng style ở file style.js</Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder="Tìm kiếm..."
+            value={searchText}
+            onChangeText={(text) => setSearchText(text)}
+          />
+  
+          <FlatList
+            data={filteredData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.name}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    );
+  };
+  
+  export default Lab8;
